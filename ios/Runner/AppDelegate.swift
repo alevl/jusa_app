2fa84f0baa2b71933754ebe578c44fcfa45e47fa
orlandorodriguez40 <orlandorodriguez40@gmail.com>
2026-03-17 23:38:51 -0400

    Configurada API Key de Google Maps para iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps // 1. Importamos la librería de mapas
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 2. Proporcionamos la API Key de Google Cloud Console
+    GMSServices.provideAPIKey("TU_CLAVE_AQUI")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
